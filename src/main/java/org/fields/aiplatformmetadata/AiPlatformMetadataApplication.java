5b770c5ed88a3cc8ac0be6a42f681c5a6ee2dc7f
zztttt <1315691850@qq.com>
2021-01-15 13:44:09 +0800

    init

---
+++ src/main/java/org/fields/aiplatformmetadata/AiPlatformMetadataApplication.java
@@ -0,0 +1,13 @@
+package org.fields.aiplatformmetadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPlatformMetadataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiPlatformMetadataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fields/aiplatformmetadata/AiPlatformMetadataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fields.aiplatformmetadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPlatformMetadataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
